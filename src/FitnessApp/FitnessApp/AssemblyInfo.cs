fa66aec6236c0577084778651a96dada691ba5c0
Fernando Soteras <fsoterasxps@outlook.com>
2021-04-24 00:14:41 -0300

    FontAwesome integration

--- src/FitnessApp/FitnessApp/AssemblyInfo.cs
+++ src/FitnessApp/FitnessApp/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("FAregular-400.otf", Alias = "FAregular400")]
+[assembly: ExportFont("FAsolid-400.otf", Alias = "FAsolid400")]
+[assembly: ExportFont("FAsolid-900.otf", Alias = "FAsolid900")]
